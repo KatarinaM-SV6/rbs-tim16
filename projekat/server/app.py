1983dc84a9de32e073a747a18d1950b0d5b961c1
Srđan Stjepanović <srdjan.stjepanovic01@gmail.com>
2024-06-24 17:25:37 +0200

    finished endpoint for inserting new acl. This feature include check in spec if acl already exist, and deleting previos acls if new has higher rank

--- projekat/server/app.py
+++ projekat/server/app.py
@@ -2,8 +2,12 @@ from flask import Flask, request, jsonify
 import consul
 import json
 import redis
+import logging
+
 
 app = Flask(__name__)
+logger = logging.getLogger(__name__)
+
 
 # Initialize Consul client
 consul_client = consul.Consul(host='localhost', port=8500)
@@ -33,13 +37,10 @@ def write_to_consul(key):
 @app.route('/consul/read/<key>', methods=['GET'])
 def read_from_consul(key):
     try:
-        # Read data from Consul
         index, data = consul_client.kv.get(key)
 
         if data is None:
             return jsonify({'error': 'Key not found'}), 404
-
-        # Convert data from JSON string
         value = json.loads(data['Value'].decode('utf-8'))
 
         return jsonify({'key': key, 'value': value}), 200
@@ -50,25 +51,104 @@ def read_from_consul(key):
 @app.route('/acl', methods=['POST'])
 def write_to_redis():
     try:
-        # Extract JSON data from the request
         data = request.get_json()
         doc = data.get('object')
         relation = data.get('relation')
         user = data.get('user')
         key = doc + '#' + relation + '@' + user
-        value = 9
+        value = 1
 
         if not key or value is None:
             return jsonify({'error': 'Key and value are required'}), 400
-
-        # Write to Redis
-        redis_client.set(key, json.dumps(value))
-
-        return jsonify({'message': 'Data written to Redis successfully'}), 200
+        
+        if check_for_curr_relations(doc, relation, user):
+            return jsonify({'message': 'Data has been written already!'}), 200
+        else:
+            redis_client.set(key, json.dumps(value))
+            return jsonify({'message': 'Data written to Redis successfully'}), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+    
+
+def read_all_keys_from_redis(doc, user):
+    keys = {'owner': doc + '#owner@' + user, 'editor': doc + '#editor@' + user,'viewer': doc + '#viewer@' + user}
+    for_check = []
+    for key in keys.keys():
+        # r.get('e').decode('utf-8')
+        curr_data = redis_client.get(keys[key])
+        # print(curr_data)
+        if not curr_data is None:
+            for_check.append(key)
+    return for_check
 
 
+def check_for_curr_relations(doc, relation, user):
+    index, data = consul_client.kv.get(doc)
+
+    if data is None:
+        return False
+    value = json.loads(data['Value'].decode('utf-8'))
+    for_check = read_all_keys_from_redis(doc, user)
+    # logger.info(for_check)
+    for key in for_check:
+        delete_if_lower_rights(relation, value, key, doc, user)
+        if relation == key:
+            return True
+        if check_computed_userset(relation, value, key):
+            return True
+    return False
+        
+
+def check_computed_userset(relation, spec, curr_relation):
+    
+    try:
+        usersets = spec['relations'][relation]['union']
+        for obj in usersets:
+            try:
+                curr_obj = obj['computed_userset']
+                print(curr_obj)
+                if not curr_obj is None:
+                    if curr_obj['relation'] == curr_relation or check_computed_userset(curr_obj['relation'], spec, curr_relation):
+                        return True
+            except:
+                pass
+        return False
+    except:
+        return False
+        # try:
+        #     usersets = spec['relations'][curr_relation]['union']
+        #     for obj in usersets:
+        #         try:
+        #             curr_obj = obj['computed_userset']
+        #             # print(curr_obj)
+        #             if not curr_obj is None:
+        #                 if curr_obj['relation'] == relation:
+        #                     redis_client.delete(doc + '#' + curr_relation + '@' + user)
+        #                     return False
+        #             return False
+        #         except:
+        #             pass
+        # except: 
+        #     return False
+        
+def delete_if_lower_rights(relation, spec, curr_relation, doc, user):
+    try:
+        usersets = spec['relations'][curr_relation]['union']
+        for obj in usersets:
+            try:
+                curr_obj = obj['computed_userset']
+                # print(curr_obj)
+                if not curr_obj is None:
+                    if curr_obj['relation'] == relation:
+                        redis_client.delete(doc + '#' + curr_relation + '@' + user)
+                        # return False
+                # return False
+            except:
+                pass
+    except: 
+        pass       # return False
+        
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
     app.run(debug=True)
